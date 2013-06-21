0bbe578927b0285c56a19ba72eddaa3b5b243f88
Christopher Gammage <gammagec@gmail.com>
2013-06-20 23:17:06 -0700

    Fixed error with enter

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 screen -S wb -X stuff 'stop'`echo -ne '\015'`
 sleep 10
-screen -S wb -X stuff '^C'
+screen -S wb -X stuff '^C'`echo -ne '\015'`
 sleep 10
-screen -S wb -X stuff './launch.sh'
+screen -S wb -X stuff './launch.sh'`echo -ne '\015'`
